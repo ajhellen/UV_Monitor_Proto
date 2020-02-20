942bc55ec1e974e7fb32275bed20ec46707fea67
Alex Hellenberg <alexhellenberg@gmail.com>
2020-02-19 19:13:15 -0700

    Initial Commit

---
+++ UV Monitor Proto/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  UV Monitor Proto
+//
+//  Created by Alex Hellenberg on 2/19/20.
+//  Copyright © 2020 Alex Hellenberg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
